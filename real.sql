86e2db94f53839afe21fa152c3a2a4b1ca8b797a
Sikalinda Patrick <sikalindapatrick@gmail.com>
2016-08-06 00:47:41 +0100

    thats database sql file to import db name napsa

---
+++ real.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 04, 2016 at 11:13 AM
+-- Server version: 5.1.53
+-- PHP Version: 5.3.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `napsa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `approval`
+--
+
+CREATE TABLE IF NOT EXISTS `approval` (
+  `approval_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fName` varchar(200) NOT NULL,
+  `llName` varchar(200) NOT NULL,
+  `pphone_number` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`approval_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `approval`
+--
+
+INSERT INTO `approval` (`approval_id`, `fName`, `llName`, `pphone_number`, `email`, `password`) VALUES
+(1, 'Roy', 'Munyelu', '260967779464', 'ceo@probasegroup.com', 'txd852168');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `init`
+--
+
+CREATE TABLE IF NOT EXISTS `init` (
+  `init_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fName` varchar(200) NOT NULL,
+  `lName` varchar(200) NOT NULL,
+  `phone_number` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`init_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `init`
+--
+
+INSERT INTO `init` (`init_id`, `fName`, `lName`, `phone_number`, `email`, `password`) VALUES
+(1, 'Lucky', 'Simoi', '260972148199', 'lucky@gmail.com', 'txd852168');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `init_file`
+--
+
+CREATE TABLE IF NOT EXISTS `init_file` (
+  `init_file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `init_id` int(11) NOT NULL,
+  `approval_id` int(11) NOT NULL,
+  `file_name` varchar(200) NOT NULL,
+  `date_initiated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `message` varchar(250) NOT NULL,
+  `level` varchar(200) NOT NULL,
+  PRIMARY KEY (`init_file_id`,`init_id`,`approval_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `init_file`
+--
+
+INSERT INTO `init_file` (`init_file_id`, `init_id`, `approval_id`, `file_name`, `date_initiated`, `message`, `level`) VALUES
+(3, 1, 0, 'demo.json', '2016-08-04 12:10:15', '', '1'),
+(4, 1, 0, 'demoerror.json', '2016-08-04 10:22:45', '', '1');
