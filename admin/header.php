b7c9fab49a3bea91b915f47b9a28ec5bc626b40b
Sikalinda Patrick <sikalindapatrick@gmail.com>
2016-08-07 00:04:34 +0100

    added admin folder

---
+++ admin/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	
+<head>
+	<title>NAPSA Initiator</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">    
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+		<meta charset="utf-8">  
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php" style="color: white;">NAPSA - Admin</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->   
+        </div>
+    </nav>		
+</head>
