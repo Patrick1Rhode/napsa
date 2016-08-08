9b872ba8dbb602bb461eac8d4c36596253230811
INNOVATION <INNOVATION>
2016-08-08 17:52:59 +0200

    made changes

---
+++ init/header.php
@@ -0,0 +1,68 @@
+	<?php
+	session_start();
+		ob_start();
+		include '../constants/db_constants.php';
+
+		$service_number = $_SESSION['service_number'];
+
+		$connect = mysqli_connect(NAPSA_HOSTNAME, NAPSA_SERVER_USERNAME, NAPSA_SERVER_PASS, NAPSA_SERVER_DB) or die('Error connecting to Server');
+		$query = "SELECT * FROM `officer` WHERE `ServiceNumber` = '$service_number'";
+		$data_collection = mysqli_query($connect, $query);
+
+		while ($row = mysqli_fetch_array($data_collection)) {
+			$rank = $row['Rank'];
+			$surname = $row['Surname'];
+			$othername = $row['OtherNames'];
+			$unit = $row['Unit'];
+			$dateofbirth = $row['DOB']; 
+			$placeofbirth = $row['POB'];
+			$nationality = $row['Nationality'];
+			$countryofbirth = $row['CountryOfBirth'];
+			$spousename = $row['SpouseName'];
+			$spousenationality = $row['SpouseNationality'];
+			$dateofattestation = $row['DateOfAttestation'];
+
+			$name = $rank .' '. $surname .' '.$othername;
+
+		}
+
+	?>
+
+	<?php
+	$query = "SELECT * FROM `securitygroup` WHERE `SecurityGroupID` = '$service_number'";
+	$data_check = mysqli_query($connect, $query) or die("Error querying database");
+	$num_rows = mysqli_num_rows($data_check);
+
+	if($num_rows >= 1){
+		while($row = mysqli_fetch_array($data_check)){
+			$permission = $row[2];
+			$permission_name = $row[1];
+
+			$_SESSION['permissions_level'] = $permission;
+			$_SESSION['permission_type'] = $permission_name;
+
+		}
+	} else {
+		echo "You have no permissions";
+	}
+	?>
+<!DOCTYPE html>
+<html>
+	
+<head>
+	<title>NAPSA Approval Dispenser</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">    
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+		<meta charset="utf-8">  
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php" style="color: white;">NAPSA</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->   
+        </div>
+    </nav>		
+</head>
--- init/index.php
+++ init/index.php
@@ -1,74 +1,67 @@
 <?php
-include "../db/db.php";
-if (Isset($_POST['submit'])) {
-//escaping mysql injection
-$email=$_POST['email'];
-$password=$_POST['password'];
-
-
-if  (!empty($email) && !empty($password)) 
-$sql="SELECT*FROM init WHERE email='$email' AND password='$password'";
-
-$result = $conn->query($sql); 
-if ($result->num_rows > 0) {
-	
-	$rows = $result->fetch_assoc();
-	$init_id=$rows['init_id'];
-	$fName = $rows['fName'];
-	$lName = $rows['lName'];
-	$init_name = $fName." ".$lName;
-
-	$time=time()+60*60*60;
-	setCookie('init_id',$init_id,$time);
-	setCookie('init_name',$init_name,$time);
-
-	ob_start(); 
-	header("location:upload.php");
-
-}
-else  {
-	//
-	echo "<div id='u'>";
-	echo "<h3 class='forgot'>You are not a member</h3>";
-	echo "</div>";
-//
-
-}
-}
-//Header File
-include 'header.php';
+	include 'header.php';
+	include '../constants/encryption_keys.php'; 
 ?>
-<body>
-  <div class="row home">
-  <div class="container">
-    <div class="col-md-3"></div>
-    <div class="col-md-6">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">Please sign in</h3>
-        </div>
-          <div class="panel-body">
-            <form role="form" action="index.php" method="post">
-                    <fieldset>
-                <div class="form-group">
-                  <input class="form-control" placeholder="E-mail" name="email" type="text">
-              </div>
-              <div class="form-group">
-                <input class="form-control" placeholder="Password" name="password" type="password">
-              </div>
-              <div class="checkbox">
-                  <label>
-                    <input name="remember" type="checkbox" value="Remember Me"> Remember Me
-                  </label>
-                </div>
-              <input class="btn btn-md btn-success btn-block" type="submit" value="Login" name="submit">
-            </fieldset>
-              </form>
-          </div>
-      </div>
-    </div>
-    <div class="col-md-3"></div>    
-  </div>
-  </div>        
-</body>
+
+<article>
+	<div class="row">
+	
+		<div class="col-md-6 col-md-push-3">
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+			<fieldset>
+				<legend>Napsa Approval Console</legend>
+				
+				<p>Service Number: <br>
+				<input type="text" name="username"></p>
+				<p>Password:<br>
+				<input type="password" name="password"></p>
+				<p><input type="submit" name="login" value="Login" class="btn btn-primary"></p>
+			</fieldset>
+		</form>
+		</div>
+	</div>
+</article>
+
 </html>
+
+<?php
+if(isset($_POST['login'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if(!empty($username) && !empty($password)){
+		$connect = mysqli_connect(NAPSA_HOSTNAME, NAPSA_SERVER_USERNAME, NAPSA_SERVER_PASS, NAPSA_SERVER_DB) or die('Error connecting to Server');
+
+		//check varialbles and sanitize them to prevent SQL Injection
+		function Sanitize_Input($input, $connect){
+			$input = mysqli_real_escape_string($connect, $input);
+			$input = htmlentities($input);
+
+			return $input;
+		}
+
+		$username = Sanitize_Input($username, $connect);
+		$password = Sanitize_Input($password, $connect);
+
+		//Encrypt your password
+		$password = hash_hmac(ALGO, $password, SYSTEM_KEY);
+
+		$query = "SELECT * FROM `login` WHERE `Password` = '$password' && `ServiceNumber` = '$username'";
+		$data_query = mysqli_query($connect, $query) or die('Error Querying Db');
+
+		$num_rows = mysqli_num_rows($data_query);
+
+		if ($num_rows >= 1) {
+			while ($row = mysqli_fetch_array($data_query)) {
+				$ServiceNumber = $row[1];
+				$_SESSION['service_number'] = $ServiceNumber;
+			}
+		
+			header('Location: upload.php');
+		} else {
+			echo "Hello No";
+		}
+	}
+}
+?>
+
